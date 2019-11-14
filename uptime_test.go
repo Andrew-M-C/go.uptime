cc8260e8f6b4c55dcddb103d804439dc2b17c09f
Andrew-M-C <laplacezhang@126.com>
2019-11-15 07:56:16 +0800

    [*] chore: Use more effecient way.

--- uptime_test.go
+++ uptime_test.go
@@ -2,11 +2,13 @@ package uptime
 
 import (
 	"testing"
+	"time"
 )
 
 func TestBasic(t *testing.T) {
+	t.Logf("now: %v", ProcUpTime())
 	for i := 0; i < 10; i++ {
 		SleepToNextSecond()
-		t.Logf("now: %v", ProcUpTime())
+		t.Logf("now: %v - %v", time.Now(), ProcUpTime())
 	}
 }
